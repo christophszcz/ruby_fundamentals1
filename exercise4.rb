00c826559986d8ce8496e8983e976acce66b4c37
Christoph Szczechowicz <christoph.szczechowicz@gmail.com>
2015-01-24 23:15:01 -0500

    Exercise 4 finally working properly

--- exercise4.rb
+++ exercise4.rb
@@ -1,11 +1,13 @@
-	one_to_hundred = (1..100)
-	one_to_hundred.each do |number|
-	puts (number).to_s
-	if number % 3 == 0 && number % 5 ==0
-	puts "Bitmaker" 
-	elsif number % 3 == 0
-	puts "Bit" 
-	elsif number % 5 == 0
-	puts "Maker" 
-	end
+one_to_hundred = (1..100)
+	
+one_to_hundred.each do |number|
+		if number % 3 == 0 && number % 5 ==0
+		puts (number="Bitmaker").to_s 
+		elsif number % 3 == 0
+		puts (number="Bit").to_s
+		elsif number % 5 == 0
+		puts (number="Maker").to_s
+		else 
+		puts number  
+	  end
 end
